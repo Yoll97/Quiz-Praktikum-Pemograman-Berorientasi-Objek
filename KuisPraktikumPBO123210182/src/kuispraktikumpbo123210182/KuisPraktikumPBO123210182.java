cd931bbc454be0e2114a6af7bb7ec99b8d57ee52
Yoll97 <127313727+Yoll97@users.noreply.github.com>
2023-03-24 15:06:47 +0700

    Add files via upload

---
+++ KuisPraktikumPBO123210182/src/kuispraktikumpbo123210182/KuisPraktikumPBO123210182.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuispraktikumpbo123210182;
+
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class KuisPraktikumPBO123210182 {
+
+    /**
+     * @param args the command line arguments
+     */
+    //public static void main(String[] args) {
+        // TODO code application logic here
+    }//
+    
+//}//
+
+class GUI extends Designer{
+    final JTextFiled fnama = new JTextField(30);
+    
+    JLabel lnama = new JLabel("Nama");
+    JLabel lasalkampus = new JLabel("Asal Kampus");
+    JLabel lipk = new JLabel("IPK");
+    JLabel lportofolio = new JLabel("Portofolio");
+    JLabel lprototyping = new JLabel ("Prototyping");
+    JLabel lKreativitas = new JLabel ("Kreativitas");
+    
+}
+
+class GUI extends Programmer{
+    final JTextFiled fnama = new JTextField(30);
+    
+    JLabel lnama = new JLabel("Nama");
+    JLabel lasalkampus = new JLabel("Asal Kampus");
+    JLabel lipk = new JLabel("IPK");
+    JLabel lKnowledge = new JLabel("Knowledge");
+    JLabel lcleancode = new JLabel ("cleancode");
+    JLabel ldebugging = new JLabel ("debugging");
+    
+}
